c147246ec26063f9ebbe4581cf61dfe355fca2ff
ljquann <76014088+ljquann@users.noreply.github.com>
2021-02-27 18:59:15 -0700

    Added new component

--- src/App.js
+++ src/App.js
@@ -1,97 +1,16 @@
 import './App.css';
 import Search from "./Search";
+import Forecast from "./Forecast";
+
 function App() {
   return (
    <body>
-<div class="container">
-    <header class="row">
+<div className="container">
+    <header>
        <Search />
     </header>
     <main>
-        <div class="today new-day row">
-            <div class="far-left col-4">
-                <h2>
-                    Today
-                </h2>
-                <h3>
-                    Time
-                </h3>
-            </div>
-            <div class="icon col-4">
-                ☔️
-            </div>
-            <div class="forcast col-4">
-                <ul>
-                    <li>Temp</li>
-                    <li>Description</li>
-                    <li>Humidity</li>
-                    <li>Wind</li>
-                </ul>
-            </div>
-        </div>
-        <div class="tomorrow new-day row">
-            <div class="far-left col-4">
-                <h2>
-                    Tomorrow
-                </h2>
-                <h3>
-                    Time
-                </h3>
-            </div>
-            <div class="icon col-4">
-                ☔️
-            </div>
-            <div class="forcast col-4">
-                <ul>
-                    <li>Temp</li>
-                    <li>Description</li>
-                    <li>Humidity</li>
-                    <li>Wind</li>
-                </ul>
-            </div>
-        </div>
-        <div class="day-after-tomorrow new-day row">
-            <div class="far-left col-4">
-                <h2>
-                    Day After Tomorrow
-                </h2>
-                <h3>
-                    Time
-                </h3>
-            </div>
-            <div class="icon col-4">
-                ☔️
-            </div>
-            <div class="forcast col-4">
-                <ul>
-                    <li>Temp</li>
-                    <li>Description</li>
-                    <li>Humidity</li>
-                    <li>Wind</li>
-                </ul>
-            </div>
-        </div>
-        <div class="day-after-that new-day row">
-            <div class="far-left col-4">
-                <h2>
-                    Day After That
-                </h2>
-                <h3>
-                    Time
-                </h3>
-            </div>
-            <div class="icon col-4">
-                ☔️
-            </div>
-            <div class="forcast col-4">
-                <ul>
-                    <li>Temp</li>
-                    <li>Description</li>
-                    <li>Humidity</li>
-                    <li>Wind</li>
-                </ul>
-            </div>
-        </div>
+        <Forecast />
     </main>
     <footer>
         <h4>This website was coded by <a href="https://www.lauraquann.com" target="_blank" rel="noreferrer">Laura Quann</a>.</h4>
