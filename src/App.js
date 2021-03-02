d87fd9db1223ce9c21537aa6b0b2ebb527c76392
ljquann <76014088+ljquann@users.noreply.github.com>
2021-03-01 17:53:48 -0700

    adding js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Search from "./Search";
-import Forecast from "./Forecast";
+import Weather from "./Weather";
 
 function App() {
   return (
       <div className="container">
-          <Search />
-        <main>
-          <Forecast />
-        </main>
+          <Weather />
       <footer>
         <h4>This <a href="https://github.com/ljquann/final-react-project" target="_blank" rel="noreferrer">weather app</a> was coded by <a href="https://www.lauraquann.com" target="_blank" rel="noreferrer">Laura Quann</a></h4>
       </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function Forecast(){
-    return (
-        <div className="forecast">
-            <div className="morning time-of-day row">
+export default function Weather() {
+  const [city, setCity] = useState(" ");
+  const [displayCity, setDisplayCity] = useState("");
+  const [todayDescription, setTodayDescription]=useState("");
+const apiKey="2ab0b590fd9866ef804df5849d5ef74a";
+  const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  function handleSubmit(event) {
+  event.preventDefault();
+  axios.get(apiUrl).then(apiCity);
+  }
+  function apiCity(response){
+ console.log(response);
+ setDisplayCity(response.data.main.name);
+ setTodayDescription(response.data.main.weather[0].main);
+ 
+ const forecastUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+ axios.get(forecastUrl).then(displayForecast);
+  }
+  function displayForecast(response){
+      console.log(response);
+
+  }
+  function changeCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="weather">
+    <header className="search row">
+        <div className="col-8 city-bar">
+        <h1> <i className="fas fa-map-marker-alt"></i> {displayCity} </h1>
+        <button className="celsius-button unit-button">°C | </button>
+        <button className="farenheit-button unit-button">°F</button>
+        </div>
+        <div className="buttons col-4">
+              <div className="dropdown">
+                  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                       <i className="fas fa-search"></i>
+                  </button>
+                  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <li className="dropdown-item">
+                                 <form onSubmit={handleSubmit} className="search-bar">
+                                    <input
+                                        type="search"
+                                        placeholder="Type a city here"
+                                        onChange={changeCity}/>
+                                    <input type="submit" className="search-button" value= "Search"></input>
+                                  </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    </header>
+    <main>
+    <div className="forecast">
+            <div className="today what-day row">
                 <div className="row-visible row">
                     <div className="col-7">
                         <h2>
-                             Description
+                             {todayDescription}
                          </h2>
                     </div>
                     <div className="col-5">
-                        <h2>Morning</h2>
-                        <h3>Time</h3>
+                        <h2>Today</h2>
                     </div>
                 </div>
         <div className="row-invisible row">
@@ -29,7 +83,7 @@ export default function Forecast(){
             </div>
         </div>
     </div>
-        <div className="midday time-of-day row">
+        <div className="tomorrow what-day row">
                 <div className="row-visible row">
                     <div className="col-7">
                         <h2>
@@ -37,8 +91,7 @@ export default function Forecast(){
                          </h2>
                     </div>
                     <div className="col-5">
-                        <h2>Midday</h2>
-                        <h3>Time</h3>
+                        <h2>Tomorrow</h2>
                     </div>
                 </div>
         <div className="row-invisible row">
@@ -55,7 +108,7 @@ export default function Forecast(){
             </div>
         </div>
     </div>
-         <div className="evening time-of-day row">
+         <div className="day-after-tomorrow what-day row">
                 <div className="row-visible row">
                     <div className="col-7">
                         <h2>
@@ -63,8 +116,7 @@ export default function Forecast(){
                          </h2>
                     </div>
                     <div className="col-5">
-                        <h2>Evening</h2>
-                        <h3>Time</h3>
+                        <h2>Day After Tomorrow</h2>
                     </div>
                 </div>
         <div className="row-invisible row">
@@ -81,7 +133,7 @@ export default function Forecast(){
             </div>
         </div>
     </div>
-         <div className="night time-of-day row">
+         <div className="day-after-that what-day row">
                 <div className="row-visible row">
                     <div className="col-7">
                         <h2>
@@ -89,8 +141,7 @@ export default function Forecast(){
                          </h2>
                     </div>
                     <div className="col-5">
-                        <h2>Night</h2>
-                        <h3>Time</h3>
+                        <h2>Day After That</h2>
                     </div>
                 </div>
         <div className="row-invisible row">
@@ -108,5 +159,7 @@ export default function Forecast(){
         </div>
     </div>
  </div>
-    );
+ </main>
+ </div>
+  );
 }
